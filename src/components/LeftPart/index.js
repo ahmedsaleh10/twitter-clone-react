e5fadc7bbbf4b3d259aadeb6309f5aa6548fc4a5
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-18 14:44:15 +0300

    mapping in the data to showing the each MenuBox.

--- src/components/LeftPart/index.js
+++ src/components/LeftPart/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { SideBarData } from '../../mockData/SidebarData';
+import MenuBox from './MenuBox';
 
 const LeftPart = () => {
   const data = SideBarData;
   console.log(data)
   return (
     <div className="sideBar">
-     
+      <img src="/assets/twitterLogo.jpg" className="twitterLogo" alt="twitter logo" />
+      {data.map((e, indx) => (
+        <MenuBox item={e} key={indx} />
+      ))}
+      <div className="btn">Post</div>
     </div>
   )
 }
