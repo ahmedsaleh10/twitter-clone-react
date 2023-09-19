42b0fc29198411756ad047464b5d0b6a5102767a
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-19 14:19:52 +0300

    pass a data as a props.

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,13 +1,12 @@
-import React from 'react'
 import LeftPart from '../LeftPart'
 import MiddlePart from '../MiddlePart'
 import RightPart from '../RightPart'
-
+import { ReactData } from '../../mockData/ReactData'
 const HomePage = () => {
   return (
     <div className='homePage'>
         <LeftPart/>
-        <MiddlePart/>
+        <MiddlePart data={ReactData}/>
         <RightPart/>
     </div>
   )
