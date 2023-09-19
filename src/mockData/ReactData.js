98179d3d69b1de42b52a5c1107c7c87f0b8524bf
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-19 14:19:24 +0300

    adding react profile data.

---
+++ src/mockData/ReactData.js
@@ -0,0 +1,15 @@
+export const ReactData = {
+    name:"React",
+    numberOfPosts:"2,610",
+    srcOfPictures: {
+        backGround:"/assets/reactPic.jpeg",
+        profile:"/assets/reactLogo.png"
+    },
+    alias:"@reactjs",
+    description:"The library for web and native user interfaces",
+    link:"https://legacy.reactjs.org",
+    dateOfJoined:"July 2013",
+    numberOfFollowing:"267",
+    numberOfFollowers:"710.1K",
+    mutualFriends:"Twitter, Instagram and 14 Others you follow"
+}
