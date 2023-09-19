f9793c1b9a4b4b52753a0fa523145ca7eee080fd
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-19 14:54:41 +0300

    prettier formating.

--- src/components/MiddlePart/index.js
+++ src/components/MiddlePart/index.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import Header from "./Header";
+import ProfilePicture from "./ProfilePicture";
+import Details from "./Details";
 
-const MiddlePart = () => {
+const MiddlePart = ({ data }) => {
   return (
-    <div>MiddlePart</div>
-  )
-}
+    <div className="middlePart">
+      <Header name={data.name} numberOfPosts={data.numberOfPosts} />
+      <ProfilePicture
+        srcOfPicture={{
+          profileBackGround: data.srcOfPictures.backGround,
+          profile: data.srcOfPictures.profile,
+        }}
+      />
+      <Details
+        name={data.name}
+        aliasName={data.alias}
+        description={data.description}
+        link={data.link}
+        dateOfJoined={data.dateOfJoined}
+        numberOfFollowing={data.numberOfFollowing}
+        numberOfFollowers={data.numberOfFollowers}
+        mutualFriends={data.mutualFriends}
+      />
+    </div>
+  );
+};
 
-export default MiddlePart
+export default MiddlePart;
