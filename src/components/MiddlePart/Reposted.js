1ea3ca1f751035691b417c9dbdac2e56dff4f14b
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-19 17:13:12 +0300

    check and return if the post is reposted by component.

---
+++ src/components/MiddlePart/Reposted.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BiRepost } from "react-icons/bi";
+
+const Reposted = () => {
+  return (
+    <div className="reposted">
+        <BiRepost /> <sub> React Reposted</sub>
+    </div>
+  )
+}
+
+export default Reposted
