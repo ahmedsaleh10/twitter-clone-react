9b446b5fa48332ed99963edce6ecde79ee49871d
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-20 20:19:13 +0300

    adding trends data.

---
+++ src/mockData/TrendsData.js
@@ -0,0 +1,47 @@
+export const TrendData = [
+    {
+        name:"#نجران الان",
+        numberOfPosts:"10.8K",
+        dir: "rtl",
+    },
+    {
+        name:"#Bitcoin",
+        numberOfPosts:"450K",
+        dir: "ltr",
+    },
+    {
+        name:"#ريال مدريد",
+        numberOfPosts:"10.5K",
+        dir: "rtl",
+    },
+    {
+        name:"#Messi",
+        numberOfPosts:"77.3K",
+        dir: "ltr",
+    },
+    {
+        name:"#ايفون",
+        numberOfPosts:"125K",
+        dir: "rtl",
+    },
+    {
+        name:"#Jenin",
+        numberOfPosts:"100K",
+        dir: "ltr",
+    },
+    {
+        name:"#القدس",
+        numberOfPosts:"950K",
+        dir: "rtl",
+    },
+    {
+        name:"#Bitcoin",
+        numberOfPosts:"450K",
+        dir: "ltr",
+    },
+    {
+        name:"#ريال مدريد",
+        numberOfPosts:"10.5K",
+        dir: "rtl",
+    },
+]
