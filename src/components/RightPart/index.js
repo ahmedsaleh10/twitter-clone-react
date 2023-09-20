0debd5c01f8b634e44445aa4c194a1ea1ac32814
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-20 20:20:00 +0300

    adding trends component to right part.

--- src/components/RightPart/index.js
+++ src/components/RightPart/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import SuggestedLiked from './SuggestedLiked'
 import {SuggestedLikedData} from "../../mockData/SuggestedLikedData"
-
+import { TrendData } from '../../mockData/TrendsData'
+import Trends from './Trends'
 const RightPart = () => {
   return (
     <div className='rightPartContainer'>
-       <SuggestedLiked data={SuggestedLikedData}/>
-      {/*<Trends /> */}
+        <SuggestedLiked data={SuggestedLikedData}/>
+        <Trends data={TrendData} />
     </div>
   )
 }
