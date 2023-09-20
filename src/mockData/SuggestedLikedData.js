e7aa781195985c1b1d628f4235016646259d9ea9
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-20 15:56:32 +0300

    adding the moc data for suggested liked.

---
+++ src/mockData/SuggestedLikedData.js
@@ -0,0 +1,22 @@
+import { FaCertificate } from "react-icons/fa";
+
+export const SuggestedLikedData = [
+    {
+      name: `Tailwind css`,
+      type: "@tailwindcss",
+      img: "/assets/tw.jpeg",
+      icon: <FaCertificate className="text-info" />,
+    },
+    {
+      name: "TypeScript",
+      type: "@typescript",
+      img: "/assets/ts.png",
+      icon: null,
+    },
+    {
+      name: "freeCodeCamp.org",
+      icon: <FaCertificate className="text-info" />,
+      type: "@freeCodeCamp",
+      img: "/assets/rf.png",
+    },
+  ]
