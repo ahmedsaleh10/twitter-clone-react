187cc68c9577d9efcfd0606627b0f0349f64cffa
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-20 15:42:13 +0300

    implement each single suggested following.

---
+++ src/components/MiddlePart/SingleSuggestedFollowing.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SingleSuggestedFollowing = ({ data }) => {
+  return (
+    <div className="SingleSuggestedFollowing">
+      <div className="sharedFollowing">
+        {data.user}
+        <small>{data.title}</small>
+      </div>
+      <div className="followingUser">
+          <img className="contentImg" src={data.img} alt="" />
+          <div className="followingUserDetails">
+            <h4 className="white-text">
+              <span className="">{data.name} </span>
+              {data.icon}
+            </h4>
+            <small>{data.type}</small>
+            <p className="white-text followingUserDescription">{data.text}</p>
+          </div>
+        <button className="followButton">Follow</button>
+      </div>
+    </div>
+  );
+};
+
+export default SingleSuggestedFollowing;
