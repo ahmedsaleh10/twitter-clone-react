95e436d7a2fcf401a2ef0083c877149274ac1a95
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-20 16:16:08 +0300

    edit the single suggested following component structure.

--- src/components/MiddlePart/SingleSuggestedFollowing.js
+++ src/components/MiddlePart/SingleSuggestedFollowing.js
@@ -8,15 +8,17 @@ const SingleSuggestedFollowing = ({ data }) => {
         <small>{data.title}</small>
       </div>
       <div className="followingUser">
+        <div className="userContentBox">
           <img className="contentImg" src={data.img} alt="" />
-          <div className="followingUserDetails">
-            <h4 className="white-text">
-              <span className="">{data.name} </span>
-              {data.icon}
-            </h4>
-            <small>{data.type}</small>
-            <p className="white-text followingUserDescription">{data.text}</p>
-          </div>
+            <div className="followingUserDetails">
+              <h4 className="white-text">
+                <span className="">{data.name} </span>
+                {data.icon}
+              </h4>
+              <small>{data.type}</small>
+              <p className="white-text followingUserDescription">{data.text}</p>
+            </div>
+        </div>
         <button className="followButton">Follow</button>
       </div>
     </div>
