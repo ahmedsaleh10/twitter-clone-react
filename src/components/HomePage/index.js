390ce6293db960d823c4470b3ca9927b47c69483
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-21 12:47:02 +0300

    comment the uneeded data.

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -2,6 +2,7 @@ import LeftPart from '../LeftPart'
 import MiddlePart from '../MiddlePart'
 import RightPart from '../RightPart'
 import { ReactData } from '../../mockData/ReactData'
+// import { TwitterData } from '../../mockData/TwitterData'
 const HomePage = () => {
   return (
     <div className='homePage'>
