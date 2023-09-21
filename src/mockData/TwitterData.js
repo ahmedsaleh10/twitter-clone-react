1aceba240b7501c400b8c820d41191dcc7efb820
Ahmed Saleh <ahmed.ovicsaleh@outlook.com>
2023-09-21 12:45:45 +0300

    adding mock twitter data.

---
+++ src/mockData/TwitterData.js
@@ -0,0 +1,15 @@
+export const TwitterData = {
+    name:"Twitter",
+    numberOfPosts:"4,307",
+    srcOfPictures: {
+        backGround:"/assets/twitterLogo.jpg",
+        profile:"/assets/twitterLogo.jpg"
+    },
+    alias:"@twitter",
+    description:"The application to share trends and twitte",
+    link:"https://legacy.reactjs.org",
+    dateOfJoined:"July 2013",
+    numberOfFollowing:"158",
+    numberOfFollowers:"821.4K",
+    mutualFriends:"React, Instagram and 14 Others you follow"
+}
